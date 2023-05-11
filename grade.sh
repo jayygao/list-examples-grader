661000ecd425899da7b329615a4fc47b857de3c8
jay <122418787+jayygao@users.noreply.github.com>
2023-05-11 09:55:50 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -14,3 +14,30 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+cd 'student-submission'
+echo "CheckPoint"
+file="ListExamples.java"
+cd '..'
+file2="TestListExamples.java"
+cd 'student-submission'
+    echo "Checkpoint 1"
+    echo $file
+    if [[ -f $file ]]
+    then 
+        cp $file '../grading-area'
+        echo "Checkpoint 2"
+        cd '..'
+        cp $file2 'grading-area'
+        cd 'grading-area'
+        javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+        java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
+        if [[ $? > 0 ]]
+        then
+            echo "Error within $file"
+        else
+            echo "Pass"
+        fi
+    else
+        echo "None"
+    fi
